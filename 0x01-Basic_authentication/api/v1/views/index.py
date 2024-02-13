69bca340722814e0ddd721df58fe5899473476e4
lawrence <lodhiambo26@gmail.com>
2024-02-13 12:44:59 +0000

    1index

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    '''
+    returns Unauthorized error.
+    '''
+    abort(401)
