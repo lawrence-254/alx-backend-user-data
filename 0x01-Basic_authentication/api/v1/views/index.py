99b720bb7fae010e808a1341b11420098ef6280f
lawrence <lodhiambo26@gmail.com>
2024-02-13 16:33:02 +0000

    2views

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> None:
     returns Unauthorized error.
     '''
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    '''
+    returns forbidden error
+    '''
+    abort(403)
