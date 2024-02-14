a3bb09dd104220b5ba57780f96cc58d863b214b3
lawrence <lodhiambo26@gmail.com>
2024-02-14 11:48:05 +0000

    cp

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''
+a class to manage the API authentication.
+'''
+import re
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    '''
+    class for authentication
+    '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+        checks if auth is required
+        '''
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        '''
+        returns auth header request
+        '''
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''
+        fetches current user
+        '''
+        return None
