8e7028735fa4f81593b5f03adca0d87cd2412732
lawrence <lodhiambo26@gmail.com>
2024-02-15 16:37:00 +0000

    4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -41,3 +41,10 @@ class Auth:
         fetches current user
         '''
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """Gets the value of the cookie named SESSION_NAME.
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
